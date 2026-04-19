2b2c67c4206312d833a191130362cf9de51c94d3
mbluejay <277542551+mbluejay@users.noreply.github.com>
2026-04-19 20:58:53 +0300

    dns: принудительно заворачиваем все DNS-запросы LAN в dnsmasq

--- xray-tproxy-setup.sh
+++ xray-tproxy-setup.sh
@@ -12,6 +12,20 @@ nft add rule inet xray_tproxy prerouting meta mark 0x000000ff accept
 nft add rule inet xray_tproxy prerouting ip daddr { 10.0.0.0/8, 172.16.0.0/12, 192.168.0.0/16, 127.0.0.0/8 } accept
 nft add rule inet xray_tproxy prerouting ip daddr 255.255.255.255 accept
 nft add rule inet xray_tproxy prerouting udp dport { 67, 68 } accept
+# DNS passthrough: чтобы DNS-хайджек (dns_hijack table) в nat prerouting успел сработать,
+# пропускаем DNS мимо tproxy не перехватывая в xray:12345
+nft add rule inet xray_tproxy prerouting iifname br-lan udp dport 53 accept
+nft add rule inet xray_tproxy prerouting iifname br-lan tcp dport 53 accept
 nft add rule inet xray_tproxy prerouting meta l4proto tcp tproxy ip to :12345 meta mark set 1
 nft add rule inet xray_tproxy prerouting meta l4proto udp tproxy ip to :12345 meta mark set 1
+
+# DNS hijack: заворачиваем все DNS-запросы от LAN-клиентов к внешним резолверам
+# на локальный dnsmasq (192.168.2.1:53). Нужно для устройств, игнорирующих DHCP DNS
+# (Xbox, Chromecast, PS5 и др.).
+nft delete table inet dns_hijack 2>/dev/null
+nft add table inet dns_hijack
+nft add chain inet dns_hijack prerouting '{ type nat hook prerouting priority dstnat; policy accept; }'
+nft add rule inet dns_hijack prerouting iifname br-lan udp dport 53 ip daddr != 192.168.2.1 dnat ip to 192.168.2.1:53
+nft add rule inet dns_hijack prerouting iifname br-lan tcp dport 53 ip daddr != 192.168.2.1 dnat ip to 192.168.2.1:53
+
 echo TPROXY_OK
